943e25aa4a9e5885f2c0321ac8581c96d4098a5b
OboyleNYC <patrick.oboyle@protonmail.com>
2025-08-17 00:22:25 -0400

    daily_python_challenge

---
+++ Daily_Python/day1.py
@@ -0,0 +1,22 @@
+#Challenge Description:
+#Create a Python program that defines three variables:
+#
+#An integer (e.g., your age).
+#
+#A float (e.g., your height in meters).
+#
+#A string (e.g., your favorite color).
+#
+#Then, print them all out in a single sentence.
+#
+#Input Example
+#(No user input; you define the values in code.)
+#
+#Expected Output
+#My age is 25, my height is 1.75 meters, and my favorite color is blue.
+
+AGE = 25
+HEIGHT = 1.75
+COLOR = "Blue"
+
+print(f"My age is {AGE}, my height is {HEIGHT} meters, and my favorite color is {COLOR}.")
